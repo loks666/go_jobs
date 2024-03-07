f0230f9c1b81ad205541a96aed473e6cdbbc23af
jax <284190056@qq.com>
2024-03-08 05:40:07 +0800

    添加驱动

--- submit/51job/main.go
+++ submit/51job/main.go
@@ -1,1 +1,59 @@
-package _1job
+package main
+
+import (
+	"fmt"
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
+	"time"
+)
+
+func main() {
+	// 启动 ChromeDriver
+	var opts []selenium.ServiceOption
+	service, err := selenium.NewChromeDriverService("path/to/chromedriver", 4444, opts...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer service.Stop()
+
+	// 连接到 Chrome
+	caps := selenium.Capabilities{"browserName": "chrome"}
+	chromeCaps := chrome.Capabilities{
+		Path: "",
+		Args: []string{
+			"--start-maximized",
+		},
+	}
+	caps.AddChrome(chromeCaps)
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", 4444))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer wd.Quit()
+
+	// 导航到页面
+	err = wd.Get("https://www.example.com")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 找到元素
+	elem, err := wd.FindElement(selenium.ByCSSSelector, "#some-id")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 操作元素
+	err = elem.SendKeys("some text")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 等待
+	time.Sleep(5 * time.Second)
+}
--- submit/boss/main.go
+++ submit/boss/main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"github.com/tebeka/selenium"
+)
+
+type Job struct {
+	Href      string
+	JobName   string
+	JobArea   string
+	Salary    string
+	Tag       string
+	Recruiter string
+}
+
+var (
+	EnableNotifications = true
+	Page                = 1
+	MaxPage             = 50
+	LoginUrl            = "https://www.zhipin.com/web/user/?ka=header-login"
+	BaseUrl             = "https://www.zhipin.com/web/geek/job?query=Java&city=101020100&page="
+	SayHi               = "您好，我有7年的工作经验，有Java，Python，Golang，大模型的相关项目经验，希望应聘这个岗位，期待可以与您进一步沟通，谢谢！"
+)
+
+func main() {
+	// Set up Selenium WebDriver.
+	caps := selenium.Capabilities{"browserName": "chrome"}
+	wd, err := selenium.NewRemote(caps, "")
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	// TODO: Implement the rest of the functionality.
+}
+
+func initDriver() {
+	// TODO: Initialize the WebDriver.
+}
+
+func resumeSubmission(url string) int {
+	// TODO: Implement the resume submission functionality.
+	return 0
+}
+
+func login() {
+	// TODO: Implement the login functionality.
+}
